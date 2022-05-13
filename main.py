e3cbe7473d21ea583659e7f07fb7b72d9b4ef449
Sylle4573 <83766674+Sylle4573@users.noreply.github.com>
2022-05-13 16:36:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import cv2
 import os
 
+### - Edit this if needed - ###
+chars   = " -.,~:=*!;#$@"   # The color ascii representation. Change if needed. More to the right means brighter.
+res   = (200, 50)           # Recommended to keep as is due to performance issues at higher resolutions!
+webcam  = 1                 # The camera to use
+### - Edit this if needed - ###
 
-here    = os.path.dirname(os.path.realpath(__file__))
-chars   = " -.,~:=*!;#$@"
-res_x = 200
-res_y = 50
 
+here = os.path.dirname(os.path.realpath(__file__))
 print("initializing cam")
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(webcam)
 print("initialized cam")
 
 
+# Captures image from webcam
 def get_image():
     result, image = cam.read()
     if result:
         return image, 1
     return 0, 0
 
+# Displays image in ascii
 def display_image(img):
     line = ""
     for x in img:
@@ -29,12 +33,18 @@ def display_image(img):
     os.system("cls")
     print(line)
 
-while True:
-    image = get_image()
-    if (image[1]):
-        image = image[0]
-        cv2.imshow("image", image)
-        cv2.waitKey(1)
-        image = cv2.resize(image, (res_x, res_y))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    display_image(image)
+# Main loop
+def main():
+    while True:
+        image = get_image()
+        if (image[1]):
+            image = image[0]
+            cv2.imshow("image", image)
+            cv2.waitKey(1)
+            image = cv2.resize(image, (res[0], res[1]))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        display_image(image)
+
+
+# If any of it needs better explanation, just message me.
+# I've got nothing better to do.
